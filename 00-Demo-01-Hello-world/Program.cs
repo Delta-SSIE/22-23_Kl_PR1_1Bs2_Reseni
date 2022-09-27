8a11556924a8cb5dad11401b7e598d5bfa5d8cd7
Jan Koupil <jan.koupil@gmail.com>
2022-09-27 13:04:52 +0200

    úvod reseni 000-090

---
+++ 00-Demo-01-Hello-world/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _00_Demo_01_Hello_world
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
